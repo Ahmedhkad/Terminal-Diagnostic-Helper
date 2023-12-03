9dd3ca8da1ca33f86c7037512b9a214c8d61729f
Ahmed Al-Omairi <alomairi@mail.ru>
2023-12-03 21:54:43 +0300

    add problem ready to copy to excel

--- js.js
+++ js.js
@@ -1,29 +1,29 @@
 let details = {
-    usb: "Восстановление ПО",
-    backcase: "Замена заднего корпуса",
-    keyboard: "Замена клавиатуры",
-    frontcase: "Замена переднего корпуса",
-    printerroller: "Замена ролика принтера",
-    printerchange: "Замена принтера",
-    printerclean: "Очистка принтера",
-    printercoverused: "Замена крышки принтера (б\y)",
-    printercovernew: "Замена крышка принтера и ролик (новая)",
-    plonka: "Снять пленку с экрана",
-    screen: "Замена дисплея",
-    newdc: "Замена разъема зарядки",
-    magreader: "Замена магнитный ридер",
-    Tarakan: "Отказ. Залит тараканами",
-    water: "Отказ. попадание жидкости",
-    simreader: "Перепайка картридера",
-    cpu: "Отказ. системная плата",
-    protection: "Перепайка тамперного модуля",
-    dcjack: "Перепайка разъема зарядки",
-    batteries: "Замена акб",
-    buzzer: "Замена пищалка",
-    conactlessboard: "Замена платы ктлц",
-    key: "Очистка клавиатуры",
-    keyC: "Очистка платы ктлц",
-    boardio: "Замена нижней платы"
+    usb: ["Восстановление ПО","TAMPER"],
+    backcase: ["Замена заднего корпуса","Корпус МЕХ"],
+    keyboard: ["Замена клавиатуры","Залипает клавиатура"],
+    frontcase: ["Замена переднего корпуса","Корпус МЕХ"],
+    printerroller: ["Замена ролика принтера","Не печатает"],
+    printerchange: ["Замена принтера","Не печатает"],
+    printerclean: ["Очистка принтера","Не печатает"],
+    printercoverused: ["Замена крышки принтера (б\y)","Не печатает"],
+    printercovernew: ["Замена крышка принтера и ролик (новая)","Не печатает"],
+    plonka: ["Снять пленку с экрана","Экран"],
+    screen: ["Замена дисплея","Экран"],
+    newdc: ["Замена разъема зарядки","Не заряжается"],
+    magreader: ["Замена магнитный ридер","Зависает"],
+    Tarakan: ["Отказ. Залит тараканами","Залит"],
+    water: ["Отказ. попадание жидкости","Залит"],
+    simreader: ["Перепайка картридера","SIM"],
+    cpu: ["Отказ. системная плата","Залит"],
+    protection: ["Перепайка тамперного модуля",""],
+    dcjack: ["Перепайка разъема зарядки","Не заряжается"],
+    batteries: ["Замена акб","TAMPER"],
+    buzzer: ["Замена пищалка","пищалка"],
+    conactlessboard: ["Замена платы ктлц","Залипает клавиатура"],
+    key: ["Очистка клавиатуры","Залипает клавиатура"],
+    keyC: ["Очистка платы ктлц","Залипает клавиатура"],
+    boardio: ["Замена нижней платы","Не печатает"]
 }
 
 let tempDetails = {}
@@ -64,18 +64,29 @@ $("#Tarakan").click(function () {
     const dancers = document.querySelector('.dancers');
     dancers.classList.toggle("visible");
     $('#titleInput').val(details.Tarakan)
+    var problem = Object.keys(tempDetails)[0]
+     $('#titleInput2').val(tempDetails[problem][1] + '	' + "Отказ. Залит тараканами")
 });
 
 $("#water").click(function () {
     $('#titleInput').val(details.water)
+    var problem = Object.keys(tempDetails)[0]
+     $('#titleInput2').val(tempDetails[problem][1] + '	' + "Отказ. попадание жидкости")
+    
 });
 
 $("#cpu").click(function () {
     $('#titleInput').val(details.cpu)
+    var problem = Object.keys(tempDetails)[0]
+     $('#titleInput2').val(tempDetails[problem][1] + '	' + "Отказ. системная плата")
 });
 
+
+
+
+
 $("#conactlessboard").click(function () {
-    tempDetails.keyC = "Замена платы ктлц"
+    tempDetails.keyC = ["Замена платы ктлц","Залипает клавиатура"]
     checkDetails()
     // $('#titleInput').val(details.conactlessboard)
 });
@@ -86,20 +97,29 @@ checkDetails = () => {
         if (Object.hasOwnProperty.call(tempDetails, key)) {
             const element = tempDetails[key];
             if (element) {
-                // console.log(key + '  -  ' + element);
-                list.push(element)
+                console.log(key + '  -  ' + element[0]);
+                list.push(element[0])
             }
         }
     }
     console.log(list);
+    var problem = Object.keys(tempDetails)[0]
+     
     var newList = list.join(',').replace(/,/g, ' + ').split();
     $('#titleInput').val(newList)
+    $('#titleInput2').val(tempDetails[problem][1] + '	' + newList)
 }
 
 
 $('#copyText').click(function () {
     // navigator.clipboard.writeText($('#titleInput').val());
     $("#titleInput").select();
+    document.execCommand('copy');
+});
+
+$('#copyText2').click(function () {
+    // navigator.clipboard.writeText($('#titleInput').val());
+    $("#titleInput2").select();
     document.execCommand('copy');
 });
 
@@ -116,6 +136,7 @@ $('#reset').on('click', () => {
         $(item).trigger('alwaysOn.maphilight');
     });
     tempDetails = {};
+    $('#titleInput2').val('Cleaned!')
 
     checkDetails();
 });
